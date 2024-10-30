b0ba4461ee8423233e10f169a5faea12b57d899e
Maximo Diodato <166886271+MaximoDiodato07@users.noreply.github.com>
2024-10-30 04:12:34 -0300

    Correcion de plantina config

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 class Config{
     
     // Database connection data
-    public static string $DB_HOST = "localhost"; // Nombre de hosting
-    public static string $DB_NAME = "isft130_catalogo" ; // Nombre de la BD
-    public static string $DB_USER = "root"; // Nombre de usario en BD
-    public static string $DB_PASSWORD = "44358439"; //Contraseña  de la BD
+    public static string $DB_HOST = ""; // Nombre de hosting
+    public static string $DB_NAME = "" ; // Nombre de la BD
+    public static string $DB_USER = ""; // Nombre de usario en BD
+    public static string $DB_PASSWORD = ""; //Contraseña  de la BD
 
 }
 
